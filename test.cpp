dcc74d9c08c9de93e4a6960d1063af70a972b53e
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-05-04 22:42:14 +0900

    test

--- test.cpp
+++ test.cpp
@@ -219,11 +219,11 @@ int main(int argc, char** argv){
     double df_inv = 1.0 - df;
     double inv_num_of_vertex = 1.0 / num_of_vertex;
     vector<double> div_send;
-    vector<double> gather_pr;
-    gather_pr.resize(num_of_vertex, 1.0/num_of_vertex);
+    //vector<double> gather_pr;
+    //gather_pr.resize(num_of_vertex, 1.0/num_of_vertex);
     //vector<double> gather_pr;
    
-    double* recv_buffer_ptr = gather_pr.data(); 
+    double* recv_buffer_ptr = recv[0].data(); 
     //const vector<vector<size_t>>& graph1 = graph;
     //const vector<int>& num_outgoing1 = num_outgoing;
     //double* div_send_ptr = div_send.data();
@@ -334,16 +334,9 @@ int main(int argc, char** argv){
                 worker[i].detach();*/
         }
         else{
-            if(rank == 0){
-                //cout << "start" << endl;
+            if(rank == 0)
                 myrdma.rdma_recv_pagerank(0);
-                //gather_pr = recv[0];
-                //MPI_Bcast(recv_buffer_ptr, num_of_vertex, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-                //cout << "end" << endl;
-            }
-            MPI_Bcast(gather_pr.data(), num_of_vertex, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-            //recv[0] = gather_pr;
-            //cout << "recv success" << endl;
+            MPI_Bcast(recv[0].data(), num_of_vertex, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         }
         clock_gettime(CLOCK_MONOTONIC, &end1);
         //time1 = (end1.tv_sec - begin1.tv_sec) + (end1.tv_nsec - begin1.tv_nsec) / 1000000000.0;

75bc578919a38bbf84fed441aa427b5610f8ad13
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-05-06 18:28:01 +0900

    test

--- test.cpp
+++ test.cpp
@@ -222,7 +222,7 @@ int main(int argc, char** argv){
     //vector<double> gather_pr;
     //gather_pr.resize(num_of_vertex, 1.0/num_of_vertex);
     //vector<double> gather_pr;
-    
+    double* recv_buffer_ptr = recv[0].data();
     //const vector<vector<size_t>>& graph1 = graph;
     //const vector<int>& num_outgoing1 = num_outgoing;
     //double* div_send_ptr = div_send.data();
@@ -260,7 +260,7 @@ int main(int argc, char** argv){
             //clock_gettime(CLOCK_MONOTONIC, &begin1);
             for(size_t i=start;i<end;i++){
                 //cout << i << endl;
-                //double* recv_buffer_ptr = recv[0].data();
+                //
                 double tmp = 0.0;
                 const size_t graph_size = graph[i].size();
                 const size_t* graph_ptr = graph[i].data();
@@ -269,7 +269,7 @@ int main(int argc, char** argv){
                     const size_t from_page = graph_ptr[j];
                     const double inv_num_outgoing = 1.0 / num_outgoing[from_page];
 
-                    tmp += recv[0][from_page] * inv_num_outgoing;
+                    tmp += recv_buffer_ptr[from_page] * inv_num_outgoing;
                 }
                 div_send[i-start] = (tmp + dangling_pr * inv_num_of_vertex) * df + df_inv * inv_num_of_vertex;
             }
@@ -316,6 +316,7 @@ int main(int argc, char** argv){
                 myrdma.rdma_write_vector(send[0],0);
                 //cout << "send success" << endl;
             }
+            recv[0].resize(num_of_vertex);
             
         }
         clock_gettime(CLOCK_MONOTONIC, &end1);

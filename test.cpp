fb86e5ca051542281e4f2a07d3b0e8a6e3d92f27
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-05-07 23:50:48 +0900

    test

--- test.cpp
+++ test.cpp
@@ -148,7 +148,7 @@ int main(int argc, char** argv){
         myrdma.create_rdma_info();
         myrdma.send_info_change_qp();
     }
-
+    MPI_Bcast(recv1[0].data(), recv1[0].size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
     // graph partitioning
     int recvcounts[size];
     int displs[size];
@@ -208,7 +208,7 @@ int main(int argc, char** argv){
 
         nn[num_of_node-2] = x;
     }
-    MPI_Bcast(recv1[0].data(), recv1[0].size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
   // cout << "end" << endl;
     int check;
     int check1[size];

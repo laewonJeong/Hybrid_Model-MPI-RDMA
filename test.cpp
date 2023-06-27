948d0c44b21c929cd5b3ddf31d267b74fa354315
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-06-27 17:02:44 +0900

    MPI_Bcast test

--- test.cpp
+++ test.cpp
@@ -460,8 +460,10 @@ int main(int argc, char** argv){
             }
 
             MPI_Allgather(&check, 1, MPI_INT, check1, 1, MPI_INT, MPI_COMM_WORLD);
+            
+            MPI_Bcast(recv1[0].data(), recv1[0].size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-            clock_gettime(CLOCK_MONOTONIC, &begin1);
+            /*clock_gettime(CLOCK_MONOTONIC, &begin1);
             if(rank == 0){
                 for(size_t dest=1; dest<size; dest++){
                     MPI_Isend(recv_buffer_ptr, num_of_vertex, MPI_DOUBLE, dest, 32548, MPI_COMM_WORLD, &request);
@@ -476,11 +478,11 @@ int main(int argc, char** argv){
             printf("%d: mpi_broadcast 수행시간: %Lfs.\n", rank, time1);
             /*if(rank == 0){
                 myrdma.rdma_recv_pagerank(0);
-            }
+            }*/
             double* recv_buffer_ptr = recv1[0].data();
             cout << recv1[0].size() << endl;
             cout << recv1[0].data() << endl;
-            MPI_Bcast(recv1[0].data(), recv1[0].size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);*/
+            
             
             
         }

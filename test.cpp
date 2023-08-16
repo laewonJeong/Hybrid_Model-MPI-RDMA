a4f5781a800b565924914725d7e112678bd9d5ac
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-08-16 19:54:45 +0900

    Tets

--- test.cpp
+++ test.cpp
@@ -194,6 +194,7 @@ int main(int argc, char** argv){
     size_t index = 0;
     int edge_part = ceil((edge/(num_of_node-1)));
     int vertex_part = ceil((num_of_vertex/(num_of_node-1)*8));
+    int part = ceil((edge+num_of_vertex*8)/(num_of_node-1));
     //cout << edge_part << endl;
     long long buffer_size = num_of_vertex * sizeof(double);
     long long buf_part = buffer_size/(num_of_node-1);
@@ -201,7 +202,7 @@ int main(int argc, char** argv){
     cout << "ve: " << ve << endl;
     for(size_t i=0;i<num_of_vertex;i++){
         temp += num_outgoing[i];
-        if( temp+ttt*8 >= vertex_part+edge_part){//+ ttt + (ttt*sizeof(double))> edge_part+vertex_part+buf_part){
+        if( temp+ttt*8 >= part){//+ ttt + (ttt*sizeof(double))> edge_part+vertex_part+buf_part){
             //cout << i << ", " << temp - num_outgoing[i] + ttt << endl;
             temp = num_outgoing[i];
             end_arr[index] = i;

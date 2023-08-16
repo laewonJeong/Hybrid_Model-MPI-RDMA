dc08c3ec5a5419c7a3a9b7f659e8ede2feed398c
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-08-16 19:24:39 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -193,7 +193,7 @@ int main(int argc, char** argv){
     int temp = 0;
     size_t index = 0;
     int edge_part = ceil(edge/(num_of_node-1));
-    int vertex_part = ceil((num_of_vertex/(num_of_node-1))*8);
+    int vertex_part = ceil((num_of_vertex/(num_of_node-1))*ve);
     //cout << edge_part << endl;
     long long buffer_size = num_of_vertex * sizeof(double);
     long long buf_part = buffer_size/(num_of_node-1);
@@ -201,7 +201,7 @@ int main(int argc, char** argv){
     cout << "ve: " << ve << endl;
     for(size_t i=0;i<num_of_vertex;i++){
         temp += num_outgoing[i];
-        if( ve-0.1 <= (temp/ttt) && (temp/ttt)<= ve+0.1){//+ ttt + (ttt*sizeof(double))> edge_part+vertex_part+buf_part){
+        if( temp+ttt*ve > edge_part+vertex_part){//+ ttt + (ttt*sizeof(double))> edge_part+vertex_part+buf_part){
             //cout << i << ", " << temp - num_outgoing[i] + ttt << endl;
             temp = num_outgoing[i];
             end_arr[index] = i;

29c393517fa816f8f49dcfc4707dc9a8aa5f15a3
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-08-16 20:13:22 +0900

    test

--- test.cpp
+++ test.cpp
@@ -182,7 +182,7 @@ int main(int argc, char** argv){
     }
     
     // graph partitioning
-    double ve = edge/num_of_vertex +1 ;
+    double ve = edge/num_of_vertex;
 
     int recvcounts[size];
     int displs[size]; 
@@ -194,7 +194,7 @@ int main(int argc, char** argv){
     size_t index = 0;
     int edge_part = ceil((edge/(num_of_node-1)));
     int vertex_part = ceil((num_of_vertex/(num_of_node-1)*8));
-    int part = ceil((edge+num_of_vertex*8)/(num_of_node-1));
+    int part = ceil((edge+num_of_vertex*ve)/(num_of_node-1));
     //cout << edge_part << endl;
     long long buffer_size = num_of_vertex * sizeof(double);
     long long buf_part = buffer_size/(num_of_node-1);
@@ -202,7 +202,7 @@ int main(int argc, char** argv){
     cout << "ve: " << ve << endl;
     for(size_t i=0;i<num_of_vertex;i++){
         temp += num_outgoing[i];
-        if( temp+ttt*8 >= part && ttt*ve >= temp){//+ ttt + (ttt*sizeof(double))> edge_part+vertex_part+buf_part){
+        if( temp+ttt*ve >= part){//+ ttt + (ttt*sizeof(double))> edge_part+vertex_part+buf_part){
             //cout << i << ", " << temp - num_outgoing[i] + ttt << endl;
             temp = num_outgoing[i];
             end_arr[index] = i;

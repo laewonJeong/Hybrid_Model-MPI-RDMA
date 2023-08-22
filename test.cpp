40ce7415b66e569fd91545002c758d7d203f3260
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-08-22 12:26:30 +0900

    test

--- test.cpp
+++ test.cpp
@@ -193,7 +193,7 @@ int main(int argc, char** argv){
     int temp = 0;
     size_t index = 0;
     int edge_part = ceil((edge/(num_of_node-1)));
-    int vertex_part = ceil((num_of_vertex/(num_of_node-1)*16));
+    int vertex_part = ceil((num_of_vertex/(num_of_node-1))*16);
     int part = ceil((edge+num_of_vertex)/(num_of_node-1));
     //cout << edge_part << endl;
     long long buffer_size = num_of_vertex * sizeof(double);

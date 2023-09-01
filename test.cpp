fa2d01aaf17910378e53d16f30a4aca4fb515d2c
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-01 18:28:01 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -169,7 +169,7 @@ int main(int argc, char** argv){
     for(int i =0; i<num_of_vertex;i++){
         long double weight = 1+num_outgoing[i];//1+log(static_cast<long double>(num_outgoing[i])+1.0); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
         vertex_weight.push_back(weight);
-        //sum_weight += weight;
+        sum_weight += weight;
     }
     double sum = 0;
     printf("%Lf\n", sum_weight);

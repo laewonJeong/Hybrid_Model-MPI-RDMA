cc66c937982fda94734c7cebfb248c65955b443c
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-01 17:39:30 +0900

    test

--- test.cpp
+++ test.cpp
@@ -167,17 +167,14 @@ int main(int argc, char** argv){
     vector<long double> vertex_weight;
     long double sum_weight = 0;
     for(int i =0; i<num_of_vertex;i++){
-        if(num_outgoing[i] == 0)
-            vertex_weight.push_back(0.0);
-        else{
-            vertex_weight.push_back(1/num_outgoing[i]);
-            sum_weight+=double(1/num_outgoing[i]);
-        }
+        long double weight = log(static_cast<long double>(num_outgoing[i]) + 1.0); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+        vertex_weight.push_back(weight);
+        sum_weight += weight;
     }
     double sum = 0;
     printf("%Lf\n", sum_weight);
     for(int i =0; i<num_of_vertex;i++){
-        vertex_weight[i] = double(vertex_weight[i]/sum_weight);
+        vertex_weight[i] /= sum_weight;
         sum += vertex_weight[i];
         if(sum >= 0.25){
             cout << sum<< " and " << i << endl;

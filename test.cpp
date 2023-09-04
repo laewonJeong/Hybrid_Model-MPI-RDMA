9459d62da425d10f5b154983c511303c90e199a5
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-04 16:46:59 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -176,7 +176,7 @@ int main(int argc, char** argv){
     vector<long double> vertex_weight;
     long double sum_weight = 0;
     for(int i =0; i<num_of_vertex;i++){
-        long double weight = log10(static_cast<long double>(num_outgoing[i]+1.0)) / log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+        long double weight = sqrt(static_cast<long double>(num_outgoing[i]+1.0)) / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
         vertex_weight.push_back(weight);
         sum_weight += weight;
     }

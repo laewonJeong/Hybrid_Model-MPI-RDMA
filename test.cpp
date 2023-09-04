2abea30a32f3938821a24cf972c85862f6bfcfb6
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-04 15:19:35 +0900

    Tets

--- test.cpp
+++ test.cpp
@@ -166,12 +166,11 @@ int main(int argc, char** argv){
 
     
 //==================================================================================
+    cout << double(edge/num_of_vertex) << endl;
     cout.precision(numeric_limits<double>::digits10);
     vector<long double> vertex_weight;
     long double sum_weight = 0;
     for(int i =0; i<num_of_vertex;i++){
-        cout << i << " " << num_outgoing[i] << endl;
-
         long double weight = 1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
         vertex_weight.push_back(weight);
         sum_weight += weight;

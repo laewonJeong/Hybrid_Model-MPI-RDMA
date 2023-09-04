9468ad02d09e01f79a778b352e8858de6632c330
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-04 16:57:58 +0900

    test

--- test.cpp
+++ test.cpp
@@ -175,16 +175,16 @@ int main(int argc, char** argv){
     cout.precision(numeric_limits<double>::digits10);
     vector<long double> vertex_weight;
     long double sum_weight = 0;
-    long double max_weight = log2(static_cast<long double>(max_edge+1.0))/max_edge;
+    long double max_weight = sqrt(static_cast<long double>(max_edge+1.0))/max_edge;
     for(int i =0; i<num_of_vertex;i++){
-        long double weight = log2(static_cast<long double>(num_outgoing[i]+1.0)) / max_edge * max_weight;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+        long double weight = sqrt(static_cast<long double>(num_outgoing[i]+1.0)) / max_edge * max_weight;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
         vertex_weight.push_back(weight);
         sum_weight += weight;
     }
     double sum = 0;
     printf("%Lf\n", sum_weight);
     for(int i =0; i<num_of_vertex;i++){
-        //vertex_weight[i] /= sum_weight;
+        vertex_weight[i] /= sum_weight;
         sum += vertex_weight[i];
         if(sum >= 0.25){
             cout << sum<< " and " << i << endl;

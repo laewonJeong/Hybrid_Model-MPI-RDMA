c0a3ea4d6c919939667342419b862f8c5bd6dcee
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-04 13:58:10 +0900

    Tets

--- test.cpp
+++ test.cpp
@@ -169,7 +169,7 @@ int main(int argc, char** argv){
     vector<long double> vertex_weight;
     long double sum_weight = 0;
     for(int i =0; i<num_of_vertex;i++){
-        long double weight = logistic(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+        long double weight = logistic(static_cast<long double>(num_outgoing[i])); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
         vertex_weight.push_back(weight);
         sum_weight += weight;
     }

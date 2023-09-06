e750d282274c0b3bc88a49bf3b00428b24875f0b
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-06 15:13:16 +0900

    test

--- test.cpp
+++ test.cpp
@@ -179,7 +179,7 @@ int main(int argc, char** argv){
     double max_weight = log(static_cast<double>(max_edge+1.0));
     
     for(int i =0; i<num_of_vertex;i++){
-        double weight = 1+sqrt(static_cast<double>(num_outgoing[i]+1));// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+        double weight = sqrt(static_cast<double>(num_outgoing[i]+1));// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
         vertex_weight.push_back(weight);
         sum_weight += weight;
     }
@@ -189,7 +189,7 @@ int main(int argc, char** argv){
     for(int i =0; i<num_of_vertex;i++){
         vertex_weight[i] /= sum_weight;
         sum += vertex_weight[i];
-        if(sum >= 0.25){
+        if(sum >= 1/(num_of_node-1)){
             cout << sum<< " and " << i << endl;
             sum = 0;
         }
@@ -243,7 +243,7 @@ int main(int argc, char** argv){
     cout << "ve: " << ve << endl;
     for(int i =0; i<num_of_vertex;i++){
         sum += vertex_weight[i];
-        if(sum >= 0.25){
+        if(sum >= 1/(num_of_node-1)){
             end_arr[index] = i;
             sum = 0;
             if(index<num_of_node-1)

efc7018e4e85cc77d9bc41cbf6191cc7248c25ee
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-06 14:15:07 +0900

    test

--- test.cpp
+++ test.cpp
@@ -172,22 +172,20 @@ int main(int argc, char** argv){
 //==================================================================================
     cout << "[INFO]AVG EDGE: "<<double(edge/num_of_vertex) << endl;
     cout << "[INFO]MAX EDGE: "<<max_edge <<endl;
+
     cout.precision(numeric_limits<double>::digits10);
     vector<long double> vertex_weight;
     long double sum_weight = 0;
     long double max_weight = log(static_cast<long double>(max_edge+1.0));
+    
     for(int i =0; i<num_of_vertex;i++){
-        if(num_outgoing[i] == 0){
-            vertex_weight.push_back(0.0);
-        }
-        else{
-            long double weight = num_outgoing[i]*sqrt(static_cast<long double>(num_outgoing[i]+1.0));// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
-            vertex_weight.push_back(weight);
-            sum_weight += weight;
-        }
+        long double weight = log(static_cast<long double>(num_outgoing[i]+1.0))/ log(static_cast<long double>(max_edge));// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+        vertex_weight.push_back(weight);
+        sum_weight += weight;
     }
     double sum = 0;
-    printf("%Lf\n", sum_weight);
+    //printf("%Lf\n", sum_weight);
+    
     for(int i =0; i<num_of_vertex;i++){
         vertex_weight[i] /= sum_weight;
         sum += vertex_weight[i];

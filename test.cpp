416e9e0eef24dc827fb23e487fbc0c6b207a9470
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-07 15:13:35 +0900

    test

--- test.cpp
+++ test.cpp
@@ -193,8 +193,9 @@ int main(int argc, char** argv){
     for(int i =0; i<num_of_vertex;i++){
         vertex_weight[i] /= sum_weight;
         sum += vertex_weight[i];
+
         if(sum >= 0.25){
-            cout << sum<< " and " << i << endl;
+            cout << sum-vertex_weight[i-1]<< " and " << i-1 << endl;
             sum = 0;
         }
         //printf("%llf\n", vertex_weight[i]);

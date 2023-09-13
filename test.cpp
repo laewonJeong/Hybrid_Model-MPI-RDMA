fb655b0252e7c3b61ebbabf1ca8cce136bc5e5ae
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-13 15:31:23 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -198,7 +198,7 @@ int main(int argc, char** argv){
     //double max_weight = log(static_cast<double>(max_edge+1.0));
     if(my_ip != node[0]){
         for(int i =0; i<num_of_vertex;i++){
-            double weight = 1;//sqrt(num_outgoing[i]+1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+            double weight = sqrt(num_outgoing[i]+1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
             vertex_weight.push_back(weight);
             sum_weight += weight;
         }
@@ -431,10 +431,8 @@ int main(int argc, char** argv){
             //cout << "nn[i]: " <<nn[i] << endl;
         }
     }
-    graph.resize(0);
-    graph.clear();
-    graph.shrink_to_fit();
-    std::vector<std::vector<size_t>>(graph).swap(graph);
+   
+    std::vector<std::vector<size_t>>().swap(graph);
     /*int div_num_of_vertex = num_of_vertex/(num_of_node-1);    
     if(my_ip == node[num_of_node-1])
         div_num_of_vertex = num_of_vertex - num_of_vertex/(num_of_node-1)*3;

e5ef105640a914c1c07d998f4946f1331a5e8761
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-13 18:36:44 +0900

    test

--- test.cpp
+++ test.cpp
@@ -197,7 +197,7 @@ int main(int argc, char** argv){
     double sum_weight = 0;
     double sum = 0;
     //if(my_ip != node[0]){
-        for(int i =0; i<num_of_vertex;i++){
+        /*for(int i =0; i<num_of_vertex;i++){
             double weight = sqrt(num_outgoing[i]+1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
             vertex_weight.push_back(weight);
             sum_weight += weight;
@@ -218,7 +218,7 @@ int main(int argc, char** argv){
     
         for(int i=0;i<5;i++){
             printf("%lf\n", vertex_weight[i]);
-        }
+        }*/
     //}
 //==================================================================================
     myRDMA myrdma;

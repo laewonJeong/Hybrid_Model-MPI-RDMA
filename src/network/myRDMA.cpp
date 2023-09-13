2ea20065b46a9b90d21b4671a47f7aa7f7f0943f
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-13 17:05:06 +0900

    Test

--- src/network/myRDMA.cpp
+++ src/network/myRDMA.cpp
@@ -372,7 +372,7 @@ void myRDMA::create_rdma_info(vector<double> *send, vector<double> *recv){
                 struct ibv_cq* completion_queue = ibv_create_cq(context, cq_size, nullptr, nullptr, 0);
                 struct ibv_qp* qp = rdma.createQueuePair(protection_domain, completion_queue);
                 struct ibv_mr *mr = rdma.registerMemoryRegion(protection_domain, 
-                                                        recv[i].data(), sizeof(recv[i][0]) * recv[i].size());//sizeof(myrdma.recv[i].data()));
+                                                        recv[i].data(), sizeof(double) * myrdma.num_of_vertex);//sizeof(myrdma.recv[i].data()));
                 uint16_t lid = rdma.getLocalId(context, PORT);
                 uint32_t qp_num = rdma.getQueuePairNumber(qp);
                 rdma_info1[j].emplace_back(RdmaInfo{context,protection_domain,cq_size,completion_queue,qp,mr,lid,qp_num});
@@ -388,7 +388,7 @@ void myRDMA::create_rdma_info(vector<double> *send, vector<double> *recv){
                 struct ibv_pd* protection_domain = ibv_alloc_pd(context);
                 struct ibv_qp* qp = rdma.createQueuePair(protection_domain, completion_queue);
                 struct ibv_mr *mr = rdma.registerMemoryRegion(protection_domain, 
-                                                        send[i].data(), sizeof(send[i][0]) * send[i].size());//sizeof(myrdma.send[i].data()));
+                                                        send[i].data(), sizeof(double) * myrdma.num_of_vertex);//sizeof(myrdma.send[i].data()));
                 uint16_t lid = rdma.getLocalId(context, PORT);
                 uint32_t qp_num = rdma.getQueuePairNumber(qp);
                 rdma_info1[j].emplace_back(RdmaInfo{context,protection_domain,cq_size,completion_queue,qp,mr,lid,qp_num});

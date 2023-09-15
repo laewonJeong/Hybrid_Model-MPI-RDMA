a4f4439acf263cd5ac06df3a00f70f8abe938558
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-15 15:07:34 +0900

    Test

--- src/lib/pagerank.cpp
+++ src/lib/pagerank.cpp
@@ -262,40 +262,41 @@ void Pagerank::graph_partition(std::vector<std::vector<size_t>>* graph,std::vect
             end += end_arr[0];
         }*/
 
-
-        vector<double> vertex_weight;
-        double sum_weight = 0;
-        double sum = 0;
-        index = 0;
-        for(int i =start; i<end;i++){
-            double weight = sqrt(num_outgoing[i]+1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
-            vertex_weight.push_back(weight);
-            sum_weight += weight;
-        }
-    
-        for(int i =start; i<end;i++){
-            vertex_weight[i] /= sum_weight;
-        }
-    
-        for(int i =start; i<end;i++){
-            sum += vertex_weight[i];
-            if(sum >= 0.25){
-                end_arr[index] = i-1;
-                sum = 0;
-                if(index<num_of_node-1)
-                    start_arr[index+1] = i-1;
-                index++;
+        if(size > 1){
+            vector<double> vertex_weight;
+            double sum_weight = 0;
+            double sum = 0;
+            index = 0;
+            for(int i =start; i<end;i++){
+                double weight = sqrt(num_outgoing[i]+1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+                vertex_weight.push_back(weight);
+                sum_weight += weight;
             }
-            if(index == num_of_node-2)
-                break;
-        //printf("%llf\n", vertex_weight[i]);
-        }
-        end_arr[num_of_node-2] = end;
-        for(int i=0;i<size;i++){
-            if(rank == i){
-                //div_num_of_vertex = end_arr[i-1] - start_arr[i-1];
-                start = start_arr[i];
-                end = end_arr[i];
+    
+            for(int i =start; i<end;i++){
+                vertex_weight[i] /= sum_weight;
+            }
+    
+            for(int i =start; i<end;i++){
+                sum += vertex_weight[i];
+                if(sum >= 0.25){
+                    end_arr[index] = i-1;
+                    sum = 0;
+                    if(index<num_of_node-1)
+                        start_arr[index+1] = i-1;
+                    index++;
+                }
+                if(index == num_of_node-2)
+                    break;
+            //printf("%llf\n", vertex_weight[i]);
+            }
+            end_arr[num_of_node-2] = end;
+            for(int i=0;i<size;i++){
+                if(rank == i){
+                    //div_num_of_vertex = end_arr[i-1] - start_arr[i-1];
+                    start = start_arr[i];
+                    end = end_arr[i];
+                }
             }
         }
         //p_sliced_graph.resize(end-start);

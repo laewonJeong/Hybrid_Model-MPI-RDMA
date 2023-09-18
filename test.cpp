3bf25f2864d4b63f0f5e71e5610676162d8f451e
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-18 18:34:53 +0900

    Test

--- test.cpp
+++ test.cpp
@@ -56,7 +56,7 @@ int main(int argc, char** argv){
     struct timespec begin2, end2 ;
     std::vector<std::vector<size_t>>* graph = new std::vector<std::vector<size_t>>();
     std::vector<std::vector<size_t>>* sliced_graph = new std::vector<std::vector<size_t>>();
-    std::vector<std::vector<size_t>> slice_graph;
+    //std::vector<std::vector<size_t>> slice_graph;
 
     vector<double> send[num_of_node];
     vector<double> recv1[num_of_node];
@@ -106,14 +106,14 @@ int main(int argc, char** argv){
     clock_gettime(CLOCK_MONOTONIC, &end1);
     long double create_graph_time = (end1.tv_sec - begin1.tv_sec) + (end1.tv_nsec - begin1.tv_nsec) / 1000000000.0;
     
-    if(my_ip != node[0]){
-        slice_graph = std::vector<std::vector<size_t>>((*sliced_graph).begin(),(*sliced_graph).end());
-        delete sliced_graph;
-    }
+    //if(my_ip != node[0]){
+    //    slice_graph = std::vector<std::vector<size_t>>((*sliced_graph).begin(),(*sliced_graph).end());
+    //    delete sliced_graph;
+    //}
     //Check Graph size==============================================================
     
     size_t innerVectorsSize = 0;
-    for (const auto& innerVector : slice_graph) {
+    for (const auto& innerVector : (*sliced_graph)) {
         innerVectorsSize += innerVector.size() * sizeof(size_t);
     }
     size_t totalSize = innerVectorsSize;
@@ -148,9 +148,9 @@ int main(int argc, char** argv){
 
     //Check sliced_graph size==========================================================
     
-    size_t s = sizeof(slice_graph); // 외부 벡터의 크기
+    size_t s = sizeof(sliced_graph); // 외부 벡터의 크기
 
-    for (const auto& innerVector : slice_graph) {
+    for (const auto& innerVector : (*sliced_graph)) {
         s += innerVector.size() * sizeof(size_t); // 내부 벡터의 크기
     }
 
@@ -272,8 +272,8 @@ int main(int argc, char** argv){
                 //
                 idx = i;
                 double tmp = 0.0;
-                const size_t graph_size = slice_graph[i].size();
-                const size_t* graph_ptr = slice_graph[i].data();
+                const size_t graph_size = (*sliced_graph)[i].size();
+                const size_t* graph_ptr = (*sliced_graph)[i].data();
                 for(size_t j=0; j<graph_size; j++){
                     const size_t from_page = graph_ptr[j];
                     const double inv_num_outgoing = 1.0 / num_outgoing[from_page];

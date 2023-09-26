89ecf34552cac04e1e56ae9465de863b4ebb3f98
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-26 10:47:33 +0900

    test

--- src/lib/pagerank.cpp
+++ src/lib/pagerank.cpp
@@ -245,7 +245,7 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
             double div_weight = 1.0/size;
 
             for(int i =start; i<end;i++){
-                double weight = sqrt(num_outgoing[i]);//sqrt(sqrt(pow(num_outgoing[i],2.7)) + 1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.                vertex_weight.push_back(weight);
+                double weight = pow(num_outgoing[i],1/1.8);//sqrt(num_outgoing[i]);//sqrt(sqrt(pow(num_outgoing[i],2.7)) + 1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.                vertex_weight.push_back(weight);
                 vertex_weight.push_back(weight);
                 sum_weight += weight;
             }

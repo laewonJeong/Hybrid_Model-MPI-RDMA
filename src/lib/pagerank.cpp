9aad2227dceaa9c571677e6af1bf258aa465f70f
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-09-26 10:43:18 +0900

    test

--- src/lib/pagerank.cpp
+++ src/lib/pagerank.cpp
@@ -182,7 +182,7 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
         double sum = 0;
         int c = 0;
         for(int i =0; i<num_vertex;i++){
-            double weight = pow(num_outgoing[i] + 1.0,1/1.4);//sqrt(num_outgoing[i]);//log(log(num_outgoing[i] + 2.0)+1.0);//log(log(num_outgoing[i]+1.0)+1.0);//sqrt(sqrt(pow(num_outgoing[i],2.8))) + 1.0;//sqrt(sqrt(pow(num_outgoing[i],2.7)) + 1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
+            double weight = pow(num_outgoing[i],1/1.6);//sqrt(num_outgoing[i]);//log(log(num_outgoing[i] + 2.0)+1.0);//log(log(num_outgoing[i]+1.0)+1.0);//sqrt(sqrt(pow(num_outgoing[i],2.8))) + 1.0;//sqrt(sqrt(pow(num_outgoing[i],2.7)) + 1.0);// / max_edge;//log10(static_cast<long double>(max_edge));//1+log(static_cast<long double>(num_outgoing[i]+1.0)); // 로그에 1을 더하여 0으로 나누는 오류를 피합니다.
             vertex_weight.push_back(weight);
             sum_weight += weight;
             //if(num_outgoing[i] == 0)

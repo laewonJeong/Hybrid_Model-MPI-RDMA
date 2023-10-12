7a13552ea4bda24239696d99db771b03b856bed2
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-10-12 15:50:22 +0900

    Test

--- src/lib/pagerank.cpp
+++ src/lib/pagerank.cpp
@@ -266,9 +266,9 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
                 else{
                     z_score = (num_outgoing[i]-avg)/std;
                     if(z_score > 1)
-                        weight = sqrt(num_outgoing[i]*round(z_score+1.0));
+                        weight = sqrt(num_outgoing[i]*(z_score+1.0));
                     else
-                        weight = sqrt(num_outgoing[i]*round(z_score+1.0));
+                        weight = sqrt(num_outgoing[i]*(z_score+1.0));
                 }
                 
             }

2bcc248e7cc575e3a3e6aafc2c1033a6f01bd45d
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-10-12 14:21:21 +0900

    Test

--- src/lib/pagerank.cpp
+++ src/lib/pagerank.cpp
@@ -266,7 +266,7 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
                 else{
                     alpha = num_outgoing[i];
                     alpha = alpha-avg;
-                    weight = sqrt(num_outgoing[i]*log(alpha+2.74));
+                    weight = sqrt(num_outgoing[i]*log(alpha+2.718281));
                 }
                 
             }

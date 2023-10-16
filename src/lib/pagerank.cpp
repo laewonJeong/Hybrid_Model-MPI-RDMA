39ceae1881976714f887ee2ecec17c4b5e89192b
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-10-16 19:49:02 +0900

    Test

--- src/lib/pagerank.cpp
+++ src/lib/pagerank.cpp
@@ -258,6 +258,7 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
         double avg;
         double std;
         double median;
+        double max;
         int c = 0;
         for(int i =0; i<num_vertex;i++){
 
@@ -274,6 +275,7 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
             else{
                 avg = 14.2362;//35.253;//2;//35.253;//14.2362;//14.2362;//2;//14.2362;//14.2362;//35.253;//+36;
                 std = 36.0803;//2419.74;//99.915;//36.0803;//36.0803;//2419.74;
+                max = 20293;
                 median = 12;//3;//12;//3;//12;//0;
                 if(num_outgoing[i] <= round(avg))//pow(std,2))
                     if(num_outgoing[i] == 0)
@@ -281,7 +283,7 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
                     else
                         weight = 1;//sqrt(num_outgoing[i]);// - (median - num_outgoing[i]) * (std / 2));
                 else{
-                    z_score = ((num_outgoing[i]-round(avg))*(1.0/floor(std)));//avg)/std;
+                    z_score = num_outgoing[i]/max;//((num_outgoing[i]-round(avg))*(1.0/floor(std)));//avg)/std;
                     //if(z_score > 1)
                     weight = sqrt(num_outgoing[i] + z_score);//num_outgoing[i]);//((num_outgoing[i] * sqrt(z_score)) * sizeof(size_t)));//num_outgoing[i] * sqrt((num_outgoing[i]-median)));//sqrt((num_outgoing[i]+1)+(num_outgoing[i] - median));//num_outgoing[i]-median));
                     //else

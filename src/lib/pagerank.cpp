331aa91dc933fdfdf4d2f2d70e01a5f444fbe200
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-10-24 13:41:36 +0900

    Test

--- src/lib/pagerank.cpp
+++ src/lib/pagerank.cpp
@@ -304,7 +304,7 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
                 std = 2419.74;//87.0887;//36.0803;//2419.74;//30.4273;//99.915;//30.4273;//6.55653;//16.356;//2419.74;//36.0803;//2419.74;//2419.74;//99.915;//36.0803;//36.0803;//2419.74;
                 max = 20293;
                 median = 12;//3;//12;//3;//12;//0;
-                if(num_outgoing[i] <= 500)//pow(std,2))
+                if(num_outgoing[i] <= 300)//pow(std,2))
                     weight = 100;//sqrt(num_outgoing[i]);// - (median - num_outgoing[i]) * (std / 2));
                 else{
                     //z_score = ((num_outgoing[i]-round(avg))*(1.0/floor(std)));//avg)/std;

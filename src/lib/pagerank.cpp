cc2a939087455f405ff1a0edd762b9f6b4890214
laewonjeong <laewonjeong@kangwon.ac.kr>
2024-08-18 19:19:34 +0900

    Test

--- src/lib/pagerank.cpp
+++ src/lib/pagerank.cpp
@@ -333,8 +333,8 @@ void Pagerank::create_vertex_weight(string path, string del, vector<int>& num_ou
                 //size_t vm = num_outgoing[i] * sizeof(size_t);
                 
                 double n_diff = 0;
-                n_diff = pow(num_outgoing[i]/round(avg), 1.5) * sizeof(size_t);
-                double unit_step_val = 1 / (1 + exp(-3 * (num_outgoing[i] - avg)));
+                n_diff = pow(num_outgoing[i]/round(avg), 1.75) * sizeof(size_t);
+                double unit_step_val = 1 / (1 + exp(-5 * (num_outgoing[i] - avg)));
                 weight = stod(alpha1) + sqrt(n_diff)*unit_step_val;//ReLU(0.0, n_diff) + stod(alpha1);//ReLU(0 , sqrt((num_outgoing[i]-round(avg))*sizeof(size_t))) + sizeof(size_t);//sqrt(num_outgoing[i]);
                 
                 
